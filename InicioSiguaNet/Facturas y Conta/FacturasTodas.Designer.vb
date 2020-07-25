3c6fdb7fdc75a1ef6c7aee25fa538c31e3ebb5a3
Hacknel <alexis.00363@outlook.com>
2020-07-25 17:34:07 -0600

    Agregado Form Todas las Facturas | 17:33 25/07 |

---
+++ InicioSiguaNet/Facturas y Conta/FacturasTodas.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FacturasTodas
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvFactuTodas = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnActualizar = New System.Windows.Forms.Button()
+        CType(Me.dgvFactuTodas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvFactuTodas
+        '
+        Me.dgvFactuTodas.AllowUserToAddRows = False
+        Me.dgvFactuTodas.AllowUserToDeleteRows = False
+        Me.dgvFactuTodas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvFactuTodas.Location = New System.Drawing.Point(10, 97)
+        Me.dgvFactuTodas.Name = "dgvFactuTodas"
+        Me.dgvFactuTodas.ReadOnly = True
+        Me.dgvFactuTodas.Size = New System.Drawing.Size(756, 310)
+        Me.dgvFactuTodas.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(301, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(150, 18)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Facturas Procesadas"
+        '
+        'btnActualizar
+        '
+        Me.btnActualizar.Location = New System.Drawing.Point(12, 68)
+        Me.btnActualizar.Name = "btnActualizar"
+        Me.btnActualizar.Size = New System.Drawing.Size(75, 23)
+        Me.btnActualizar.TabIndex = 2
+        Me.btnActualizar.Text = "Actualizar"
+        Me.btnActualizar.UseVisualStyleBackColor = True
+        '
+        'FacturasTodas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(775, 421)
+        Me.Controls.Add(Me.btnActualizar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvFactuTodas)
+        Me.Name = "FacturasTodas"
+        Me.Text = "FacturasTodas"
+        CType(Me.dgvFactuTodas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvFactuTodas As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnActualizar As Button
+End Class
---
+++ InicioSiguaNet/Facturas y Conta/FacturasTodas.vb
@@ -0,0 +1,18 @@
+﻿Public Class FacturasTodas
+    Dim Conexion As Conexion = New Conexion
+    Private Sub FacturasTodas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Conexion.LlenarDGV(dgvFactuTodas, "select f.idFactura as 'ID Factura', f.numeroIdentidadC as 'Numero Identidad', CONCAT(dp.nombres, ' ', dp.primerApellido, ' ', dp.segundoApellido) as 'Nombre', 
+			pli.nombrePaquete as 'Nombre Paquete', pli.precio as 'Precio', f.fechaPago, f.mesPago as 'Mes Pago'  from FACTURA f
+			inner join CLIENTES cl on f.numeroIdentidadC = cl.numeroIdentidad
+			inner join DATOS_PERSONAS dp on cl.numeroIdentidad = dp.numeroIdentidad
+			inner join PLANES_INTERNET pli on cl.idPaquete = pli.idPaquete where cl.estadoC <> 'Eliminado'")
+    End Sub
+
+    Private Sub btnActualizar_Click(sender As Object, e As EventArgs) Handles btnActualizar.Click
+		Conexion.LlenarDGV(dgvFactuTodas, "select f.idFactura as 'ID Factura', f.numeroIdentidadC as 'Numero Identidad', CONCAT(dp.nombres, ' ', dp.primerApellido, ' ', dp.segundoApellido) as 'Nombre', 
+			pli.nombrePaquete as 'Nombre Paquete', pli.precio as 'Precio', f.fechaPago, f.mesPago as 'Mes Pago'  from FACTURA f
+			inner join CLIENTES cl on f.numeroIdentidadC = cl.numeroIdentidad
+			inner join DATOS_PERSONAS dp on cl.numeroIdentidad = dp.numeroIdentidad
+			inner join PLANES_INTERNET pli on cl.idPaquete = pli.idPaquete where cl.estadoC <> 'Eliminado'")
+	End Sub
+End Class
